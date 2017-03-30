92ed88201639b3f70b7e4a16f762ac92ea132491
huanghuaqian <hqqinternet@gmail.com>
2017-03-30 23:53:10 +0800

    创业通知部分，import template->pages/common/itemList

---
+++ pages/index/notice/notice.js
@@ -0,0 +1,27 @@
+// pages/index/notice/notice.js
+Page({
+  data:{
+    notice: {
+      url: "/pages/common/itemDetails/itemDetails",
+      imgSrc: "http://upload.cxcycf.com/upload/201612/06/201612061512317998.jpg",
+      itemTitle: "我省创新高校编制管理 高层次人才编制可单列",
+      itemInfo: "近日，四川省委办公厅印发《关于进一步加强和改进高等学校党的建设的意见》，提出高校党政领导班子实行任期制，明确每届任期5年，领导人员原则上担任同一职务时间不超过两届或10年。&amp;emsp;&amp;emsp;《意见》强调…",
+      releaseTime: "2016-09-05 14:23"
+    }
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
