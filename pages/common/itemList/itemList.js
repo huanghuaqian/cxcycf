8915e5d41addd8becfa9c5f495755a574cb66b4a
huanghuaqian <hqqinternet@gmail.com>
2017-04-01 19:16:49 +0800

    列表项展示模板

---
+++ pages/common/itemList/itemList.js
@@ -0,0 +1,29 @@
+// pages/index/notice/notice.js
+Page({
+  data:{
+    item: {
+        url: "/pages/index/notice/noticeDetails/noticeDetails",
+    imgSrc: "http://upload.cxcycf.com/upload/201612/06/201612061512234092.jpg",
+    itemTitle: "全省扶贫开发项目档案业务培训会在蓉举办",
+    itemInfo: "8月23日，全省扶贫开发项目档案业务培训会在成都举办，旨在学习宣传贯彻《四川省扶贫开发项目档案管理细则》，进一步提高扶贫开发项目档案业务水平，扎实推进扶贫开发项目档案工作，更好地服务脱贫攻坚战略，发挥档案…",
+    releaseTime: "2016-09-05 14:24"
+    }
+    
+
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
